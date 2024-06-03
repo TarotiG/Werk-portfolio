bf6cf4f10f6130c06819be5c9a722aa13e3858fb
TarotiG <82974942+TarotiG@users.noreply.github.com>
2024-06-03 13:36:00 +0000

    coconuts.py verplaatst
    -

--- webapp/app.py
+++ webapp/app.py
@@ -1,34 +1,6 @@
 from flask import Flask, render_template
-import requests
-# from scripts.plants import get_coconuts
+from scripts.coconuts import get_coconuts
 
-def get_coconuts() -> list:
-    api_token = "sMX03TE0zXt9tpkax_FjcVA1VI-pLh-JMW7GSNk6AO8"
-    req = requests.get(f"https://trefle.io/api/v1/plants/search?token={api_token}&q=coconut")
-
-    coconuts = []
-
-    if req.status_code == 200:
-        coconut_data = req.json()
-
-        for type_coconut in coconut_data["data"]:
-            coconut = {
-                "id": type_coconut["id"],
-                "naam": type_coconut["common_name"],
-                "afbeelding_url": type_coconut["image_url"]
-            }
-
-            if coconut["naam"] is None:
-                coconut["naam"] = coconut_data["data"][0]["synonyms"][0]
-
-            for i in range(len(coconut_data["data"])):
-
-                if coconut in coconuts:
-                    continue
-                else:
-                    coconuts.append(coconut)
-
-    return coconuts
 
 app = Flask(__name__)
 coconuts = get_coconuts()
